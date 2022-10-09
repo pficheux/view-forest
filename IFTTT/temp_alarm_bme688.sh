f5a1d544c1c6674d48a45a6d6e9682f6da3605bf
Pierre FICHEUX <pierre.ficheux@smile.fr>
2022-10-09 17:40:12 +0200

    Add alarm scripts for BME688

--- IFTTT/temp_alarm_bme688.sh
+++ IFTTT/temp_alarm_bme688.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 #
-# Get the temperature from a MPC9808 sensor and send an alarm with IFTTT
+# Get the temperature from a BME688 sensor and send an alarm with IFTTT
 # Don't send the alarm if already sent before SEND_DELAY
 #
 #set -x
 
-# Running in demo mode (x86)
-DEMO=1
-
-TMAX=30
+TMAX=25
 SLEEP_T=5
 DATE_ALARM=0
 DATE_C=0
@@ -16,27 +13,17 @@ SEND_DELAY=60
 TDEG_O=0
 
 # Sensor ID
-SENSOR_ID=Pissos_1
+SENSOR_ID=$(hostname)
 
 # IFTTT parameters
 KEY=nyGTOHaJMnkXJhgIVHrxPaL6CFuCDgLMhcSAbBc7ozr
 EVENT=gmail
 
-if [ $DEMO -eq 0 ]; then
-    F=/sys/class/hwmon/hwmon0/temp1_input
-else
-    echo "Running in demo mode !"
-    F=/tmp/t
-fi
-
-echo "Using file $F"
-
 while [ 1 ];
       do
 	  # Get the temp value
-	  T=$(cat $F)
+	  TDEG=$(echo $(bme688 -t) | cut -d ' ' -f1 | cut -d '.' -f1)
 	  TDEG_O=$TDEG
-	  TDEG=$(expr $T / 1000)
 	  DATE_C=$(date +%s)
 	  if [ $TDEG -gt $TMAX ]; then
 	      D=$(expr $DATE_C - $DATE_ALARM)
@@ -45,9 +32,7 @@ while [ 1 ];
 	      if [ $D -gt $SEND_DELAY ]; then
   	          echo "*** Sending ALARM ($TDEG / $TDEG_O °C) !!!"
 		  DATE_ALARM=$(date +%s)
-		  if [ $DEMO -eq 0 ]; then
-		      curl -k -X POST -H "Content-Type: application/json" -d '{"value1":"sensor_id = '$SENSOR_ID'", "value2":"temperature = '$TDEG' °C"}' https://maker.ifttt.com/trigger/$EVENT/with/key/$KEY
-		  fi    
+		  curl -k -X POST -H "Content-Type: application/json" -d '{"value1":"sensor_id = '$SENSOR_ID'", "value2":"temperature = '$TDEG' °C"}' https://maker.ifttt.com/trigger/$EVENT/with/key/$KEY
 	      else
 		  echo "alarm already sent !"
 	      fi
@@ -56,5 +41,3 @@ while [ 1 ];
 	  fi 
 	  sleep $SLEEP_T
 done
-
-
